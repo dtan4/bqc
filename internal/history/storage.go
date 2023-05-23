1fcfd53580412406c9816ad34b3bc9e21a4233a9
Daisuke Fujita <dtanshi45@gmail.com>
2023-05-24 03:34:14 +0900

    internal/history: Register some types for gob

--- internal/history/storage.go
+++ internal/history/storage.go
@@ -1,9 +1,22 @@
 package history
 
-import "github.com/dtan4/bqc/internal/bigquery"
+import (
+	"encoding/gob"
+	"time"
+
+	bigqueryapi "cloud.google.com/go/bigquery"
+
+	"github.com/dtan4/bqc/internal/bigquery"
+)
 
 type Storage interface {
 	Close() error
 	Append(result *bigquery.Result) error
 	List() ([]*bigquery.Result, error)
+}
+
+func init() {
+	gob.Register(map[string]bigqueryapi.Value{})
+	gob.Register([]bigqueryapi.Value{})
+	gob.Register(time.Time{})
 }
